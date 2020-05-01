f619679fa7d74284a5d6a327bec168d41d088cd6
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2020-05-01 09:53:45 -0400

    Raised gapi auth to App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,109 @@
 import React from 'react';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+const SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-      </div>
-    </Router>
-  )
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.GoogleAuth = null;
+		this.initClient = this.initClient.bind(this);
+		this.handleAuthClick = this.handleAuthClick.bind(this);
+		this.setSigninStatus = this.setSigninStatus.bind(this);
+	}
+
+	componentDidMount() {
+		const googleScript = document.createElement('script');
+
+		googleScript.src = 'https://apis.google.com/js/api.js';
+		googleScript.async = true;
+		googleScript.onload = () => this.handleClientLoad();
+
+		this.instance.appendChild(googleScript);
+	}
+
+	handleClientLoad() {
+		window.gapi.load('client:auth2', this.initClient);
+	}
+
+	initClient() {
+		const discoveryURL = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
+
+		// Initialize gapi.client
+		window.gapi.client.init({
+			'apiKey': 'AIzaSyAW-UGcXN8-v8qbu9NpUmnsELBNs5Kp0T4',
+			'clientId': '15399846948-gut7ja6158h95pesfn8il1dej16rh519.apps.googleusercontent.com',
+			'discoveryDocs': [discoveryURL],
+			'scope': SCOPE
+		}).then(
+			() => {
+				this.GoogleAuth = window.gapi.auth2.getAuthInstance();
+
+				// Listen for sign-in state changes, and update signin status.
+				this.GoogleAuth.isSignedIn.listen(this.setSigninStatus);
+
+				// Handle initial sign-in state
+				// let user = this.GoogleAuth.currentUser.get(); // What is this for?
+				this.setSigninStatus();
+			}
+		);
+	} // End of initClient
+
+	setSigninStatus() {
+		let user = this.GoogleAuth.currentUser.get();
+		let isAuthorized = user.hasGrantedScopes(SCOPE);
+
+		if (isAuthorized) {
+			console.log('You\'re signed in!');
+			// this.setState({loggedIn: true});
+		}
+		else {
+			console.log('You are signed out.')
+		}
+	}
+
+	handleAuthClick() {
+		if (this.GoogleAuth.isSignedIn.get()) {
+			this.GoogleAuth.signOut();
+		}
+		else {
+			this.GoogleAuth.signIn();
+		}
+	}
+
+	getSubs() {
+		window.gapi.client.youtube.subscriptions.list({
+			'part': 'snippet',
+			// 'maxResults': 50,  // Ensure this works if user has <50 subs
+			'mine': true
+		})
+			.then(function (response) {
+				// Handle the results here (response.result has the parsed body).
+				console.log("Response", response);
+			},
+				function (err) { console.error("Execute error", err); });
+	}
+
+	render() {
+		return (
+			<Router>
+				<div className="App" ref={el => (this.instance = el)}>
+					<Switch>
+						<Route exact path="/" render={(props) => <Login {...props} handleAuthClick={this.handleAuthClick} getSubs={this.getSubs} />} />
+
+						<Route path="/dashboard" component={Dashboard} />
+					</Switch>
+				</div>
+			</Router>
+		)
+	}
 }
 
 
--- src/Login.js
+++ src/Login.js
@@ -1,101 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-// import { GoogleLogin } from 'react-google-login';
 import { Redirect } from 'react-router-dom';
 
-const SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
 
-
-class App extends React.Component {
+class Login extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = { loggedIn: false };
-
-        this.GoogleAuth = null;
-
-        // this.handleClientLoad = this.handleClientLoad.bind(this);
-        this.initClient = this.initClient.bind(this);
-        this.handleAuthClick = this.handleAuthClick.bind(this);
-        this.setSigninStatus = this.setSigninStatus.bind(this);
-    }
-
-    componentDidMount() {
-        const googleScript = document.createElement('script');
-
-        googleScript.src = 'https://apis.google.com/js/api.js';
-        googleScript.async = true;
-        googleScript.onload = () => this.handleClientLoad();
-
-        this.instance.appendChild(googleScript);
-    }
-
-    handleClientLoad() {
-        window.gapi.load('client:auth2', this.initClient);
-    }
-
-    initClient() {
-        const discoveryURL = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
-
-        // Initialize gapi.client
-        window.gapi.client.init({
-            'apiKey': 'AIzaSyAW-UGcXN8-v8qbu9NpUmnsELBNs5Kp0T4',
-            'clientId': '15399846948-gut7ja6158h95pesfn8il1dej16rh519.apps.googleusercontent.com',
-            'discoveryDocs': [discoveryURL],
-            'scope': SCOPE
-        }).then(
-            () => {
-                this.GoogleAuth = window.gapi.auth2.getAuthInstance();
-
-                // Listen for sign-in state changes, and update signin status.
-                this.GoogleAuth.isSignedIn.listen(this.setSigninStatus);
-
-                // Handle initial sign-in state
-                // let user = this.GoogleAuth.currentUser.get(); // What is this for?
-                this.setSigninStatus();
-            }
-        );
-    } // End of initClient
-
-    setSigninStatus() {
-        let user = this.GoogleAuth.currentUser.get();
-        let isAuthorized = user.hasGrantedScopes(SCOPE);
-
-        if (isAuthorized) {
-            console.log('You\'re signed in!');
-            // this.setState({loggedIn: true});
-        }
-        else {
-            console.log('You are signed out.')
-        }
-    }
-
-    handleAuthClick() {
-        if (this.GoogleAuth.isSignedIn.get()) {
-            this.GoogleAuth.signOut();
-        }
-        else {
-            this.GoogleAuth.signIn();
-        }
-    }
-
-    getSubs() {
-        window.gapi.client.youtube.subscriptions.list({
-            'part': 'snippet',
-            // 'maxResults': 50,  // Ensure this works if user has <50 subs
-            'mine': true
-        })
-            .then(function (response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-            },
-                function (err) { console.error("Execute error", err); });
     }
 
     render() {
-
         if (!this.state.loggedIn) {
             return (
                 <div className="App" ref={el => (this.instance = el)}> {/* ref tag allows for the script to be added */}
@@ -107,15 +23,15 @@ class App extends React.Component {
 
                         <p>Sign in with your Google account to get started.</p>
 
-                        <button onClick={this.handleAuthClick}>Sign in with Google</button>
+                        <button onClick={this.props.handleAuthClick}>Sign in with Google</button>
 
                         <br/>
 
                         {/* TODO see https://developers.google.com/identity/branding-guidelines for CSS guidelines */}
 
-                        {/* <button onClick={() => this.setState({ loggedIn: true })}>Skip login</button> */}
+                        <button onClick={() => this.setState({ loggedIn: true })}>Skip login</button>
 
-                        <button onClick={this.getSubs}>Fetch subscriptions</button>
+                        <button onClick={this.props.getSubs}>Fetch subscriptions</button>
 
                     </header>
                 </div>
@@ -126,4 +42,4 @@ class App extends React.Component {
 }
 
 
-export default App;
+export default Login;
