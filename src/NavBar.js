d30405e835c2f86841d4935a914178fcd3e51e26
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2020-06-08 08:44:24 -0400

    Removed disabled prop of logout btn

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 function NavBar(props) {
-    return  (
+    return (
         <Navbar>
             <Navbar.Brand>YouTag</Navbar.Brand>
-            <Button variant="outline-danger" size="sm" onClick={props.handleAuthClick} disabled={!props.isSignedIn}>Logout</Button>
+            <Button variant="outline-danger" size="sm" onClick={props.handleAuthClick}>Logout</Button>
+
+            <Link to='/dashboard'>
+                <Button size="sm">Back to Topics</Button>
+            </Link>
         </Navbar>
     );
 }
